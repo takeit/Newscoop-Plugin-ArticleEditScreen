e7572e7d8e8875ef9f911a3b01d25a991c7cf3b8
Rafał Muszyński <rmuszynski1@gmail.com>
2014-11-17 16:24:38 +0100

    AENV-257 - Locked articles can be loaded

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,30 +6,59 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\Generator\UrlGenerator;
-use Newscoop\EventDispatcher\Events\GenericEvent;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/admin/editor_plugin", options={"expose":true}, name="newscoop_admin_aes")
+     * @Route("/admin/editor_plugin/{language}/{articleNumber}", options={"expose":true}, name="newscoop_admin_aes")
      * @Template()
      */
-    public function adminAction(Request $request)
-    {   
-		$em = $this->container->get('em');
-		$preferencesService = $this->container->get('system_preferences_service');
-		$client = $em->getRepository('\Newscoop\GimmeBundle\Entity\Client')->findOneByName('newscoop_aes_'.$preferencesService->SiteSecretKey);
+    public function adminAction(Request $request, $language = null, $articleNumber = null)
+    {
+        $em = $this->container->get('em');
+        $preferencesService = $this->container->get('system_preferences_service');
+        $translator = $this->get('translator');
 
-        return array('clientId'=>$client->getPublicId());
+        if (null !== $articleNumber) {
+            $article = $em->getRepository('Newscoop\Entity\Article')
+                ->getArticle($articleNumber, $language)
+                ->getOneOrNullResult();
+
+            if (!$article) {
+                return $this->render("NewscoopEditorBundle:Alerts:alert.html.twig", array(
+                    'message' => $translator->trans('aes.alerts.notfound'),
+                    'locked' => false
+                ));
+            }
+
+            if ($article->isLocked()) {
+                $timeDiffrence = $article->getLockTimeDiffrence();
+
+                return $this->render("NewscoopEditorBundle:Alerts:alert.html.twig", array(
+                    'message' => $translator->trans('aes.alerts.locked', array(
+                        '%realname%' => $article->getLockUser()->getRealName(),
+                        '%username%' => $article->getLockUser()->getUsername(),
+                        '%hours%' => $timeDiffrence['hours'],
+                        '%minutes%' => $timeDiffrence['minutes']
+
+                    )),
+                    'locked' => true
+                ));
+            }
+        }
+
+        $client = $em->getRepository('\Newscoop\GimmeBundle\Entity\Client')->findOneByName('newscoop_aes_'.$preferencesService->SiteSecretKey);
+
+        return array(
+            'clientId' => $client->getPublicId()
+        );
     }
 
     /**
      * @Route("/bundles/newscoopeditor/views/toolbar/navigation.html")
      */
     public function navAction(Request $request)
-    {   
+    {
         return $this->render("NewscoopNewscoopBundle::admin_menu.html.twig");
     }
 }
